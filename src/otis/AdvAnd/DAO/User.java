881bd7682238091b632b0ca72049200e2dff7ea6
Ohardwick <hardwiog@mail.uc.edu>
2014-03-06 18:34:22 -0500

    Creation of DAO and passing of informaiton to check user

--- src/otis/AdvAnd/DAO/User.java
+++ src/otis/AdvAnd/DAO/User.java
@@ -2,15 +2,50 @@ package otis.AdvAnd.DAO;
 
 public class User {
 
+	public String name;
 	public String username;
 	public int imageResourse;
 	public String mPhone;
 	
-	public User(String id, int imageResource, String phoneNumber){
-		this.username = id;
-		this.imageResourse = imageResource;
-		this.mPhone = phoneNumber;
+	public String getName() {
+		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getImageResourse() {
+		return imageResourse;
+	}
+
+	public void setImageResourse(int imageResourse) {
+		this.imageResourse = imageResourse;
+	}
+
+	public String getmPhone() {
+		return mPhone;
+	}
+
+	public void setmPhone(String mPhone) {
+		this.mPhone = mPhone;
+	}
+
+	
+	
+//	public User(String id, int imageResource, String phoneNumber){
+//		this.username = id;
+//		this.imageResourse = imageResource;
+//		this.mPhone = phoneNumber;
+//	}
 	
 	public String toString() {
 		return username;
--- src/otis/AdvAnd/shareme/CheckUser.java
+++ src/otis/AdvAnd/shareme/CheckUser.java
@@ -1,5 +1,8 @@
 package otis.AdvAnd.shareme;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,11 +10,14 @@ import otis.AdvAnd.DAO.User;
 import otis.AdvAnd.DAO.UserData;
 import otis.AdvAnd.NFCController.NFCMain;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,10 +25,13 @@ public class CheckUser extends Activity {
 	
 	
 	String name;
+	String actualname;
 	String mPhone;
 	TextView checkUser;
 	TextView checkPhone;
+	EditText nameHolder;
 	User user;
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +45,13 @@ public class CheckUser extends Activity {
 		mPhone = B.getString("val2");
 		
 		
-		if (!NfcAdapter.getDefaultAdapter(this).isEnabled())
+		
+		if (!NfcAdapter.getDefaultAdapter(this).isEnabled()) // changed to check if user file exist if not create
 	    {
 	        Toast.makeText(getApplicationContext(), "Please activate NFC and press Back to return to the application!", Toast.LENGTH_LONG).show();
 	        startActivity(new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS));
 	    }
+		
 		
 		
 		checkUser = (TextView) findViewById(R.id.Title);
@@ -49,6 +60,9 @@ public class CheckUser extends Activity {
 		
 		checkPhone = (TextView) findViewById(R.id.phoneTitle);
 		checkPhone.setText(mPhone);
+		
+		nameHolder = (EditText) findViewById(R.id.Name);
+		actualname = nameHolder.getEditableText().toString();
 		
 		
 		Button begin = (Button) findViewById(R.id.Login);
@@ -61,10 +75,12 @@ public class CheckUser extends Activity {
 		@Override
 		public void onClick(View v) {
 			// Iterate through information (not set yet)
-			
-			
-			
-			
+			User creation = new User();
+			creation.setUsername(name);
+			creation.setmPhone(mPhone);
+			creation.setName(actualname);
+			UserData save = new UserData();
+			save.setUser(creation);
 			
 			
 			
